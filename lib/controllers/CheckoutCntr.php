1a1ec940a4e723768c91f65f1e305003e8c074a4
skivuha <skivuha@hotmail.com>
2015-04-07 04:36:16 -0700

    Update CheckoutCntr.php

--- lib/controllers/CheckoutCntr.php
+++ lib/controllers/CheckoutCntr.php
@@ -49,7 +49,7 @@ class CheckoutCntr implements iController
     {
         if(false === $this->data->getUser())
         {
-            header("Location: /Regestration/logon/");
+            header("Location: /~user2/PHP/shop/Regestration/logon/");
         }
     }
 }
